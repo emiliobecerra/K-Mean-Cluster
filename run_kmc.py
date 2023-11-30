ede5740b1ad50db05e4a30bed28e0914c2aa8a94
Emilio Becerra <emiliobecerra@126.221.21.198.tigernet.wifi.dyn.clemson.edu>
2023-11-30 11:25:17 -0500

    refractoring

--- run_kmc.py
+++ run_kmc.py
@@ -4,6 +4,7 @@ from sklearn.cluster import KMeans
 
 from sklearn_extra.cluster import KMedoids
 
+
 from sklearn.metrics import silhouette_score 
 
 dataset = pandas.read_csv("dataset.csv")
@@ -23,10 +24,19 @@ def run_kmeans(n, dataset):
   results = machine.predict(dataset)
   centroids = machine.cluster_centers_
   pyplot.scatter(dataset[:,0],dataset[:,1], c=results)
-  pyplot.scatter(centroids[:,0], centroids[:,1], c="red", marker="*", s=200)
+  pyplot.scatter(centroids[:,0], centroids[:, 1], c="red", marker="*", s=300)
   pyplot.savefig("scatterplot_kmeans_" + str(n) + ".png")
   pyplot.close()
   return silhouette_score(dataset, results, metric="euclidean")
+
+
+n_list = [2,3,4,5,6,7,8]
+silhouette_score_list = [run_kmeans(i, dataset) for i in n_list]
+
+pyplot.scatter(n_list, silhouette_score_list)
+pyplot.savefig("silhouette_score_kmeans.png")
+pyplot.close()
+
 
 
 
@@ -38,16 +48,31 @@ def run_kmedoids(n, dataset):
   results = machine.predict(dataset)
   centroids = machine.cluster_centers_
   pyplot.scatter(dataset[:,0],dataset[:,1], c=results)
-  pyplot.scatter(centroids[:,0], centroids[:,1], c="red", marker="*", s=200)
+  pyplot.scatter(centroids[:,0], centroids[:, 1], c="red", marker="*", s=300)
   pyplot.savefig("scatterplot_kmedoids_" + str(n) + ".png")
   pyplot.close()
   return silhouette_score(dataset, results, metric="euclidean")
 
+n_list = [2,3,4,5,6,7,8]
+silhouette_score_list = [run_kmedoids(i, dataset) for i in n_list]
 
-silhouette_score_list = [run_kmedoids(i, dataset) for i in [2,3,4,5,6,7,8]]
-
-
-
-pyplot.scatter([2,3,4,5,6,7,8], silhouette_score_list)
+pyplot.scatter(n_list, silhouette_score_list)
 pyplot.savefig("silhouette_score_kmedoids.png")
 pyplot.close()
+
+
+
+
+
+  
+  
+  
+
+
+
+
+
+
+
+
+
